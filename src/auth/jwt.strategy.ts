c54835f287a9042d33e4aa6eea117d8224fa4136
ya <p.zhilcov@mail.ru>
2022-02-07 19:01:15 +0300

    wip auth

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,10 +1,9 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { ConfigService } from "@nestjs/config";
-import { TokenService } from "src/token/token.service";
-import {User} from "../user/entities/user.entity";
-
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { TokenService } from 'src/token/token.service';
+import { User } from '../user/entities/user.entity';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -21,11 +20,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(req, user: Partial<User>) {
     const token = req.headers.authorization.slice(7);
-    // const tokenExists = await this.tokenService.exists(user._id, token);
-    // if (tokenExists) {
-    //   return user;
-    // } else {
-    //   throw new UnauthorizedException();
-    // }
+    const tokenExists = await this.tokenService.exists(user.id, token);
+    if (tokenExists) {
+      return user;
+    } else {
+      console.log(232323);
+      throw new UnauthorizedException();
+    }
   }
 }
--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,28 +1,41 @@
-import {Body, Controller, Get, Post, Query, ValidationPipe} from '@nestjs/common';
-import {CarService} from "./services/car.service";
-import {CarModelDto} from "./dto/car-model.dto";
-import {CarModel} from "./entities/car-model.entity";
-import {ApiTags} from "@nestjs/swagger";
-import {CarBrandDto} from "./dto/car-brand.dto";
-import {CarBrand} from "./entities/car-brand.entity";
-import {CarDto} from "./dto/car-dto";
-import {CarEntity} from "./entities/car.entity";
-import {ListItemInterface} from "../interfaces/list-item-interface";
-import {CarModelsListDto} from "./dto/car-models-list";
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CarService } from './services/car.service';
+import { CarModelDto } from './dto/car-model.dto';
+import { CarModel } from './entities/car-model.entity';
+import { ApiTags } from '@nestjs/swagger';
+import { CarBrandDto } from './dto/car-brand.dto';
+import { CarBrand } from './entities/car-brand.entity';
+import { CarDto } from './dto/car-dto';
+import { CarEntity } from './entities/car.entity';
+import { ListItemInterface } from '../interfaces/list-item-interface';
+import { CarModelsListDto } from './dto/car-models-list';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @ApiTags('car')
 @Controller('car')
 export class CarController {
-  constructor(private readonly carService: CarService) {
-  }
+  constructor(private readonly carService: CarService) {}
 
   @Post('create-car-model')
-  async createCarModel(@Body(ValidationPipe) carModel: CarModelDto): Promise<CarModel> {
+  async createCarModel(
+    @Body(ValidationPipe) carModel: CarModelDto,
+  ): Promise<CarModel> {
     return this.carService.createCarModel(carModel);
   }
 
   @Post('create-car-brand')
-  async createCarBrand(@Body(ValidationPipe) brand: CarBrandDto): Promise<CarBrand> {
+  @UseGuards(JwtAuthGuard)
+  async createCarBrand(
+    @Body(ValidationPipe) brand: CarBrandDto,
+  ): Promise<CarBrand> {
     return this.carService.createCarBrand(brand);
   }
 
@@ -32,12 +45,15 @@ export class CarController {
   }
 
   @Get('get-car-brands-list')
+  @UseGuards(JwtAuthGuard)
   async getCarBrandsList(): Promise<Array<ListItemInterface>> {
     return this.carService.getCarBrandsList();
   }
 
   @Get('get-car-models-list')
-  async getCarModelsList(@Query() queryParams: CarModelsListDto): Promise<Array<ListItemInterface>> {
+  async getCarModelsList(
+    @Query() queryParams: CarModelsListDto,
+  ): Promise<Array<ListItemInterface>> {
     return this.carService.getCarModels(queryParams.brandId);
   }
 }
